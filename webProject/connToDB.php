6706579efc4507349b10e76f2cf20fee13796b9e
maras-st <stavrosmaras@gmail.com>
2015-06-25 18:55:52 +0300

    Update connToDB.php

--- webProject/connToDB.php
+++ webProject/connToDB.php
@@ -1,9 +1,9 @@
 <?php
 
 	$hostname="localhost"; //local server name default localhost
-	$usernameDB="root";  //mysql username default is root.
-	$passwordDB="20/5/1993Mast";       
-	$database="webProjectDB";  //database name which you created
+	$usernameDB="";  //mysql username default is root.
+	$passwordDB="";       
+	$database="";  //database name which you created
 
 	$error=''; // Variable To Store Error Message
 	
